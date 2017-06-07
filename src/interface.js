a6f2c889954c7223a8694b84556ff17d9cae9a33
elenamorton <elena.uleia@gmail.com>
2017-06-07 17:19:23 +0100

    Adding first ajax call for London weather.

--- src/interface.js
+++ src/interface.js
@@ -27,6 +27,10 @@ $(document).ready(function(){
     updateTemperature();
   });
 
+  $.get('http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
+  $('#current-temperature').text(data.main.temp);
+})
+    
   function updateTemperature() {
     $('#temperature').text(thermostat.temperature);
     $('#temperature').attr('class', thermostat.getCurrentEnergyUsage());
