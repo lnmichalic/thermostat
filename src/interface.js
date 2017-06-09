9d2dfd879d18927b31b13c620a243884d398bb3a
Lubos Michalic <lnmichalic@gmail.com>
2017-06-09 11:24:44 +0100

    Select city changes background image

--- src/interface.js
+++ src/interface.js
@@ -35,7 +35,21 @@ $('#current-city').change(function() {
   $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
     $('#current-temperature').text(data.main.temp);
   })
-  $('#selected-city').text($('#current-city').name());
+  // $.ajax({
+  //   type: 'GET',
+  //   url: "https://api.gettyimages.com/v3/search/images/creative?phrase=+london",
+  //   beforeSend: function(request) {
+  //     request.setRequestHeader("Api-Key", "nakry66tmg85e64uetcz3vm3");
+  //   }
+  var image_url = 'https://www.googleapis.com/customsearch/v1?q='+city+'+cityscape&searchType=image&fileType=jpg&imgSize=huge&num=1&key=AIzaSyCG8bW3rCS6pZ-7vM36CGffLamocsOykuY&cx=000312491013124124462:k_gkpvyhgqa'
+  console.log(image_url);
+  $.get(image_url, function(response) {
+      console.log(response);
+      var image_url = response.items[0].link;//.imageobject[0].url;
+      $('body').css({background : 'url('+image_url+')  no-repeat fixed bottom left'});
+      // $('#current-city-pic').text(response.items[0].pagemap.imageobject[0].url);
+  })
+  // $('#selected-city').text($('#current-city').name());
 })
 
 
